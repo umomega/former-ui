40ceed593b9cf8aafa143ad9fbd298960048a9ec
Egemen Berker Kızılcan <egemenkizilcan@gmail.com>
2021-01-19 14:30:53 +0300

    Added the main js

---
+++ former-ui.js
@@ -0,0 +1,27 @@
+import AnswersEdit from './js/views/answers/AnswersEdit'
+import AnswersIndex from './js/views/answers/AnswersIndex'
+
+import FormFieldsCreate from './js/views/fields/FormFieldsCreate'
+import FormFieldsEdit from './js/views/fields/FormFieldsEdit'
+
+import FormsCreate from './js/views/forms/FormsCreate'
+import FormsEdit from './js/views/forms/FormsEdit'
+import FormsFields from './js/views/forms/FormsFields'
+import FormsIndex from './js/views/forms/FormsIndex'
+
+import AnswerDestroyed from './js/events/AnswerDestroyed'
+import AnswersDestroyedBulk from './js/events/AnswersDestroyedBulk'
+import AnswerUpdated from './js/events/AnswerUpdated'
+import FormDestroyed from './js/events/FormDestroyed'
+import FormDuplicated from './js/events/FormDuplicated'
+import FormFieldDestroyed from './js/events/FormFieldDestroyed'
+import FormFieldStored from './js/events/FormFieldStored'
+import FormFieldUpdated from './js/events/FormFieldUpdated'
+import FormsDestroyedBulk from './js/events/FormsDestroyedBulk'
+import FormStored from './js/events/FormStored'
+import FormUpdated from './js/events/FormUpdated'
+
+export { 
+	AnswersEdit, AnswersIndex, FormFieldsCreate, FormFieldsEdit, FormsCreate, FormsEdit, FormsFields, FormsIndex,
+	AnswerDestroyed, AnswersDestroyedBulk, AnswerUpdated, FormDestroyed, FormDuplicated, FormFieldDestroyed, FormFieldStored, FormFieldUpdated, FormsDestroyedBulk, FormStored, FormUpdated
+}
